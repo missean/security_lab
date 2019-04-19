41c6c59c1c94ec88bf7a70058d6f4d109bccbc2e
mwang33 <mengqi_wang@brown.edu>
2019-04-18 23:21:02 -0400

    escape2

--- memos.js
+++ memos.js
@@ -19,7 +19,7 @@ var db = require("./database.js");
 
 function addMemos(req,res,next)
 {
-   var memo = decodeURL(escape(req.body.memo));
+   var memo = escape(req.body.memo);
 
    var q = "INSERT INTO Memos(memo) VALUES ( '" + memo + "' )";
    db.query(q,function(e1,d1) { addMemos1(req,res,next,e1,d1); });
